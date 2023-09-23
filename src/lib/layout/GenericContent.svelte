a5f9f8822fc8c16d66075188ef194cd076663c10
ladyofcode <thelab@ladyofcode.com>
2023-09-23 21:08:47 +1000

    Create GenericContent component

---
+++ src/lib/layout/GenericContent.svelte
@@ -0,0 +1,39 @@
+<div class="generic-container">
+	<slot />
+</div>
+
+<style>
+	.generic-container {
+		max-width: 800px;
+		margin: 6.4rem auto;
+
+		padding: 0 1.6rem;
+	}
+
+	.generic-container :global(figure) {
+		max-height: 96vh;
+	}
+
+	.generic-container :global(figure > img) {
+		max-height: 80vh;
+		margin: 0 auto;
+	}
+
+	.generic-container :global(img),
+	.generic-container :global(video),
+	.generic-container :global(iframe) {
+		max-width: 100%;
+	}
+
+	.generic-container :global(iframe) {
+		width: 100%;
+		height: 480px;
+	}
+
+	@media (min-width: 900px) {
+		.generic-container {
+			padding: 0;
+			margin: 8rem auto;
+		}
+	}
+</style>

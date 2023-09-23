c2d6b84b60d59788fb24004781e8b25227331799
ladyofcode <thelab@ladyofcode.com>
2023-09-23 21:08:57 +1000

    Create Learn More Box component

---
+++ src/lib/components/LearnMoreBox.svelte
@@ -0,0 +1,56 @@
+<div class="learn-more-box">
+	<slot />
+</div>
+
+<style>
+	.learn-more-box {
+		background-color: hsla(37, 10%, 36%, 0.041);
+		display: flex;
+		flex-direction: column;
+		margin-top: 24px;
+		margin-bottom: 24px;
+		padding: 1.4rem 2.4rem;
+		border-radius: var(--corner-radius);
+	}
+
+	.learn-more-box :global(div) {
+		padding-left: 3.2rem;
+	}
+
+	.learn-more-box :global(h3) {
+		margin-top: 0;
+	}
+
+	.learn-more-box :global(ul) {
+		list-style: none;
+		padding: 0;
+	}
+
+	.learn-more-box :global(li a) {
+		padding: 0;
+		margin: 0;
+	}
+
+	.learn-more-box :global(a) {
+		font-family: var(--font-headings);
+		font-size: 1.2em;
+		padding: 0 0 1rem 0;
+	}
+
+	@media (min-width: 600px) {
+		.learn-more-box {
+			max-height: 160px;
+			flex-direction: row;
+			align-items: center;
+			justify-content: start;
+		}
+
+		.learn-more-box :global(img) {
+			max-width: 240px;
+		}
+
+		.learn-more-box :global(a) {
+			padding: 0 0 0 1rem;
+		}
+	}
+</style>

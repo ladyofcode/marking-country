9338366a4542257f320eb1244453cc611d66d209
ladyofcode <thelab@ladyofcode.com>
2023-09-26 21:33:29 +1000

    Add Photoswipe simple component

---
+++ src/lib/components/PhotoswipeSimple.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { onMount } from 'svelte';
+	import PhotoSwipeLightbox from 'photoswipe/lightbox';
+	import 'photoswipe/style.css';
+	export let galleryId;
+	export let images;
+
+	onMount(() => {
+		let lightbox = new PhotoSwipeLightbox({
+			gallery: '#' + galleryId,
+			children: 'a',
+			initialZoomLevel: 'fit',
+			secondaryZoomLevel: 6,
+			// maxZoomLevel: 6,
+			pswpModule: () => import('photoswipe')
+		});
+		lightbox.init();
+	});
+</script>
+
+<div class="pswp-gallery" id={galleryId}>
+	{#each images as image}
+		<a
+			href={image.largeURL}
+			data-pswp-width={image.width}
+			data-pswp-height={image.height}
+			target="_blank"
+			rel="noreferrer"
+		>
+			<img src={image.thumbnailURL} alt="" />
+		</a>
+	{/each}
+</div>

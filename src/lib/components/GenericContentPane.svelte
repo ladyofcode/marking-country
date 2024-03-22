b6067a66677575a132d96f948a1d6d9944a88c66
ladyofcode <thelab@ladyofcode.com>
2024-03-22 16:07:27 +0000

    Create GenericContentPane

---
+++ src/lib/components/GenericContentPane.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { onMount, onDestroy } from 'svelte';
+    import { gsap } from 'gsap';
+	// import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+	import { ScrollTrigger } from 'gsap/ScrollTrigger';
+	gsap.registerPlugin(ScrollTrigger);
+
+	let element, animation;
+
+	// onMount(() => {
+	// 	animation = gsap.timeline({
+	// 		scrollTrigger: {
+	// 			trigger: "#container",
+	// 			pin: true,
+	// 			start: "top top",
+	// 			pinSpacing:false,
+	// 			// end: "+=100%",
+	// 			markers: true,
+	// 		}
+	// 	});
+	// });
+
+	// onDestroy(() => {
+	// 	animation.scrollTrigger.kill(true);
+	// 	animation.kill();
+	// });
+</script>
+
+<section id="container">
+	<slot name="first"  />
+	<slot />
+</section>
+<style>
+
+</style>

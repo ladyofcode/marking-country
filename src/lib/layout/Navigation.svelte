735f1f896d530c28811da357092b725572ca1394
ladyofcode <thelab@ladyofcode.com>
2024-03-26 04:33:38 +0000

    Rebuild completely; markup iterators, overlays, etc

--- src/lib/layout/Navigation.svelte
+++ src/lib/layout/Navigation.svelte
@@ -1,228 +1,276 @@
 <script>
 	import { base } from '$app/paths';
-	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+	import { onMount, onDestroy } from 'svelte';
 
-	let current = '';
 	let menuChecked = false;
-	let handleMenuToggle;
+	let isLargeScreen = false;
+	let ariaHidden = false;
+	let hasInteracted = false;
+	let handleSubNav;
+	let currentOpenSubnav = null;
+	const largeScreen = window.matchMedia('(min-width: 900px)');
+
+	function closeNav(event) {
+		if (event && event.target.getAttribute('href') !== '#') {
+			menuChecked = false;
+			currentOpenSubnav = null;
+		}
+	}
+
+	$: $page.path, closeNav();
+
+	$: {
+		if (menuChecked && isLargeScreen=== false) {
+			document.body.style.overflow = 'hidden';
+		} else {
+			document.body.style.overflow = '';
+		}
+	}
+
+	let menuItems = [
+		{ title: 'Home', url: `${base}/` },
+		{
+			title: 'About',
+			url: `${base}#`,
+			subnavId: 'subnav1',
+			subItems: [
+				{ title: 'Marking Country', url: `${base}/about` },
+				{ title: 'Educational resources', url: `${base}/educational-resources` }
+			]
+		},
+		{ title: 'Mapping', url: `${base}/mapping` },
+		{
+			title: 'Deep Histories',
+			url: `${base}#`,
+			subnavId: 'subnav2',
+			subItems: [
+				{ title: 'Carnarvon', url: `${base}/carnarvon-gorge` },
+				{ title: 'Western Yalanji', url: `${base}/western-yalanji` },
+				{ title: 'Willandra', url: `${base}/willandra-lakes` },
+				{ title: 'Woddordda & Ngarinyin', url: `${base}/woddordda-ngarinyin-intro` },
+				{ title: 'Yawuru', url: `${base}/yawuru` }
+			]
+		}
+	];
+
+	// Initial check
+	let handleMatchMedia = (event = largeScreen) => {
+		if (event.matches) {
+			isLargeScreen = true;
+			menuChecked = true;
+			ariaHidden = true;
+		} else {
+			isLargeScreen = false;
+			menuChecked = false;
+			ariaHidden = false;
+		}
+	};
 
 	onMount(() => {
-		const largeScreen = window.matchMedia('(min-width: 900px)');
+		// Add listener for changes
+		handleMatchMedia();
+		largeScreen.onchange = (event) => {
+			handleMatchMedia(event);
+		};
 
-		if (largeScreen) {
-			menuChecked = false;
-		}
-
-		handleMenuToggle = () => {
-			menuChecked ? menuChecked = false : menuChecked = true;
-		}
+		handleSubNav = (nav, event) => {
+			if (nav.length > 0) {
+				event.preventDefault();
+			}
+			currentOpenSubnav = currentOpenSubnav === nav ? null : nav;
+		};
 	});
 </script>
 
-<header>
-	<div class="wrapper">
-		<a href="{base}/" class="logo"> Home </a>
-
+<div class="wrapper">
+	<div class="top-bar" aria-hidden={ariaHidden}>
 		<input
 			type="checkbox"
 			class="menu-button"
-			id="menuButton"
+			id="menu-button"
 			bind:checked={menuChecked}
-			on:click={handleMenuToggle}
+			on:click={() => (menuChecked = !menuChecked)}
 		/>
-		<label for="menuButton" class="menu-label">
+		<label for="menu-button">
 			<div class="hamburger">
-				<div class="hamburger">
-					<span class="line line1" />
-					<span class="line line2" />
-					<span class="line line3" />
-				</div>
+				<span class="line line1" />
+				<span class="line line2" />
+				<span class="line line3" />
 			</div>
 		</label>
-		<nav>
-			<ul class="menu">
-				<li class="subnav">
-					<a href="{base}#"> About </a>
-					<ul class="subnav-content">
-						<li>
-							<a href="{base}/about" class:active={current === ''} on:click={handleMenuToggle}>
-								Marking Country
-							</a>
-						</li>
-						<li>
-							<a
-								href="{base}/educational-resources"
-								class:active={current === ''}
-								on:click={handleMenuToggle}
-							>
-								Educational resources
-							</a>
-						</li>
-					</ul>
-				</li>
-				<li>
-					<a href="{base}/mapping" class:active={current === ''} on:click={handleMenuToggle}>
-						Mapping
-					</a>
-				</li>
-				<li class="subnav">
-					<a href="{base}#"> Deep Histories </a>
-					<ul class="subnav-content">
-						<li>
-							<a
-								href="{base}/carnarvon-gorge"
-								class:active={current === ''}
-								on:click={handleMenuToggle}
-							>
-								Carnarvon
-							</a>
-						</li>
-						<li>
-							<a
-								href="{base}/western-yalanji"
-								class:active={current === ''}
-								on:click={handleMenuToggle}
-							>
-								Western Yalanji</a
-							>
-						</li>
-						<li>
-							<a
-								href="{base}/willandra-lakes"
-								class:active={current === ''}
-								on:click={handleMenuToggle}
-							>
-								Willandra
-							</a>
-						</li>
-						<li>
-							<a
-								href="{base}/woddordda-ngarinyin-intro"
-								class:active={current === ''}
-								on:click={handleMenuToggle}
-							>
-								Woddordda & Ngarinyin
-							</a>
-						</li>
-						<li>
-							<a href="{base}/yawuru" class:active={current === ''} on:click={handleMenuToggle}>
-								Yawuru
-							</a>
-						</li>
-					</ul>
-				</li>
-			</ul>
-		</nav>
+		<div class="top-bar-underlay" class:open={menuChecked} aria-hidden={ariaHidden} />
 	</div>
-</header>
+	<nav class:open={menuChecked} aria-label="Main Navigation Menu">
+		<ul>
+			{#each menuItems as { title, url, subnavId, subItems }}
+				<li>
+					<a
+						href={url}
+						on:click={(event) => {
+							handleSubNav(subnavId ?? '', event);
+							closeNav(event);
+						}}
+					>
+						{title}
+
+						{#if url.includes('#')}
+							<div
+								class="arrow {currentOpenSubnav === subnavId ? 'down' : ''}"
+								aria-hidden="true"
+							/>
+						{/if}
+					</a>
+					{#if subItems}
+						<ul
+							class="subnav-content"
+							class:open={currentOpenSubnav === subnavId}
+							aria-label={`Submenu ${title}`}
+						>
+							{#each subItems as { title, url }}
+								<li>
+									<a href={url} on:click={(event) => closeNav(event)}>
+										{title}
+									</a>
+								</li>
+							{/each}
+						</ul>
+					{/if}
+				</li>
+			{/each}
+		</ul>
+	</nav>
+</div>
+<div
+	class="overlay"
+	class:open={menuChecked && !isLargeScreen}
+	on:click={() => (menuChecked = false)}
+	aria-hidden="true"
+/>
 
 <style>
-	header {
-		background-color: black;
+	.wrapper {
 		position: fixed;
-		height: 64px;
-		top: 0;
-		left: 0;
 		right: 0;
-		z-index: 1020;
-		width: 100%;
-		max-width: 100vw;
+		height: 100%;
+		min-height: 100vh;
+		z-index: 200;
 	}
 
-	.wrapper {
+	.overlay {
 		width: 100%;
-		max-width: 800px;
-		margin: 0 auto;
+		height: 200vh;
 		position: absolute;
 		top: 0;
-		left: 0;
 		right: 0;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		background-color: #000000;
-	}
-	.wrapper a {
-		text-decoration: none;
-		display: block;
-		padding: 1.2rem 1.6rem;
-		width: 100%;
+		z-index: 100;
+		background-color: rgba(0, 0, 0, 0.39);
+		visibility: hidden;
+		opacity: 0;
+		cursor: pointer;
+		pointer-events: none;
 	}
 
-	.wrapper a:hover {
-		background-color: #2a2a2a;
-		font-weight: bold;
+	.overlay.open {
+		opacity: 1;
+		visibility: visible;
+		transition: opacity 1s ease-in-out;
+		pointer-events: auto;
 	}
-	.wrapper ul {
-		list-style-type: none;
-		padding: 0;
+
+	.top-bar {
+		height: 70px;
 	}
-	.logo {
-		z-index: 300;
-		font-size: 1.12rem;
+
+	.top-bar-underlay {
+		display: block;
+		height: 70px;
+		width: 280px;
+		background-color: var(--clr-dark-charcoal);
+		transition: transform 0.3s ease-in-out;
+		transform: translateX(100%);
+	}
+
+	nav.open {
+		transform: translateX(0);
+	}
+	.top-bar-underlay.open {
+		transform: translateX(0);
 	}
 
 	nav {
-		position: absolute;
-		height: 64px;
-		margin: 0 auto;
-		top: 100%;
-		left: 0;
-		right: 0;
-		overflow: hidden;
-		max-height: 0;
-		transition: max-height 0.5s ease-out;
-	}
-
-	nav a:first-of-type {
-		float: left;
-	}
-
-	.menu {
-		position: relative;
+		height: 100%;
+		width: 100%;
 		display: flex;
 		flex-direction: column;
-		background-color: black;
+		overflow-x: hidden;
+		background-color: var(--clr-dark-charcoal);
+		transition: transform 0.3s ease-in-out;
+		transform: translateX(100%);
 	}
 
-	nav ul li {
-		width: 100%;
-		position: relative;
-	}
-	nav ul li:focus-within > ul,
-	.wrapper nav ul li:hover > ul {
-		display: initial;
+	ul {
+		list-style: none;
+		padding: 0;
 	}
 
-	nav ul li ul {
-		position: relative;
-		left: 0;
-		width: 200px;
-		background-color: black;
+	li a {
+		display: block;
+		padding: 1.6rem;
+		text-decoration: none;
+	}
+
+	li a:hover {
+		background-color: grey;
+	}
+
+	.subnav-content {
 		display: none;
 	}
 
-	nav > ul > li > ul > li > a {
-		width: 100%;
+	.subnav-content.open {
+		display: block;
+	}
+
+	.arrow {
+		display: block;
+		float: right;
+		border: solid var(--clr-terracotta-dark);
+		border-width: 0 3px 3px 0;
+		padding: 3px;
+		margin-top: 10px;
+		transform: rotate(45deg);
+	}
+
+	.arrow.down {
+		transform: rotate(135deg);
+		transition: transform 0.3s ease-in-out;
 	}
 
 	.menu-button {
 		display: none;
 	}
-	.menu-label {
+
+	label {
 		display: initial;
+		position: fixed;
+		top: 16px;
+		right: 16px;
+		background: black;
+		padding: 8px;
+		z-index: 200;
+		transition: background-color 0.2s ease-in-out;
 	}
 
 	.hamburger {
-		height: 52px;
-		width: 40px;
-		margin-right: 12px;
-		margin-left: auto;
-		padding: 8px;
-		position: relative;
+		cursor: pointer;
+		width: 28px !important;
+		height: 24px;
+		right: 16px;
+		top: 16px;
 		display: flex;
-		flex-direction: column;
 		justify-content: space-between;
+		flex-direction: column;
 	}
 
 	.hamburger:hover {
@@ -231,85 +279,112 @@
 
 	.line {
 		display: block;
-		height: 4px;
-		width: 100%;
 		border-radius: 10px;
+		height: 3px;
+		width: 100%;
+		transition: 0.5s;
 		background-color: #fff;
 	}
 
-	.line1 {
-		transform-origin: 0% 0%;
-		transition: transform 0.4s ease-in-out;
-	}
-	.line2 {
-		transition: transform 0.2s ease-in-out;
-	}
-	.line3 {
-		transform-origin: 0% 100%;
-		transition: transform 0.4s ease-in-out;
-	}
-
-	/* .active {
-		text-decoration: underline !important;
-		font-weight: bold;
-		text-decoration-color: #e96f24 !important;
-		text-decoration-thickness: 4px !important;
-	} */
-
-	.menu-button:checked ~ nav {
-		max-height: 100%;
-		overflow: visible;
-	}
-	.menu-button:checked ~ .menu-label .hamburger .line1 {
-		transform: rotate(45deg);
+	.menu-button:checked ~ label .hamburger .line1 {
+		transform: translateY(9px) rotate(45deg);
 		background-color: #fff;
 	}
-	.menu-button:checked ~ .menu-label .hamburger .line2 {
-		transform: scaleY(0);
+	.menu-button:checked ~ label .hamburger .line2 {
+		opacity: 0;
 		background-color: #fff;
 	}
-	.menu-button:checked ~ .menu-label .hamburger .line3 {
-		transform: rotate(-45deg);
+	.menu-button:checked ~ label .hamburger .line3 {
+		transform: translateY(-12px) rotate(-45deg);
 		background-color: #fff;
 	}
 
-	.subnav-content {
-		/* background-color: var(--color-dark-charcoal); */
-		border-bottom: 2px solid grey;
+	.menu-button:checked ~ label {
+		background-color: transparent;
 	}
 
 	@media (min-width: 900px) {
-		.wrapper a {
-			width: fit-content;
+		.wrapper {
+			min-height: 0;
+			height: fit-content;
+			width: 100%;
+			display: flex;
+			justify-content: center;
+			overflow:visible;
 		}
 
-		.wrapper nav a {
+		.overlay {
+			display: none;
+		}
+
+		.top-bar {
+			background-color: var(--clr-dark-charcoal);
 			width: 100%;
+			position: fixed;
+		}
+
+		input,
+		label,
+		.top-bar-underlay {
+			display: none;
 		}
 
 		nav {
-			top: 0;
-			overflow: visible;
+			height: fit-content;
+			flex-direction: row;
+			width: 100%;
+			max-width: 1024px;
+			overflow:visible;
+			background-color: transparent;
+			transform: translateX(0%);
 		}
+		
+		/* .wrapper:hover, nav:hover {
+			height: 100%;
+		} */
 
-		.menu {
+		nav > ul {
+			height: fit-content;
+			width: 100%;
+			display: flex;
 			flex-direction: row;
 			justify-content: flex-end;
 		}
 
-		nav ul li ul {
+		nav > ul > li:first-of-type {
+			margin-right: auto;
+		}
+
+		li {
+			height: fit-content;
+		}
+
+		li a {
+			padding: 1.4rem;
+		}
+
+		.subnav-content {
+			background-color: var(--clr-dark-charcoal);
 			position: absolute;
-			top: 64px;
-		}
-		nav ul li {
-			width: fit-content;
+			top: 70px;
+			z-index: 400;
 		}
 
-		nav ul li ul li {
-			width: 100%;
+		.arrow {
+			margin-left: 20px;
 		}
 
-		.hamburger {
+		li:hover .arrow {
+			transform: rotate(135deg);
+			transition: transform 0.3s ease-in-out;
+		}
+
+		nav > ul > li > a:hover ~ .subnav-content,
+		.subnav-content:hover {
+			display: block;
+		}
+
+		.subnav-content.close {
 			display: none;
 		}
 	}

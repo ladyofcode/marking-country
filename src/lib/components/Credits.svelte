41c48851920385b9d3903fc90fd7420a3f44a330
ladyofcode <thelab@ladyofcode.com>
2024-03-26 04:31:27 +0000

    Add animation

--- src/lib/components/Credits.svelte
+++ src/lib/components/Credits.svelte
@@ -1,11 +1,66 @@
 <script>
 	export let credits;
+
+	import { onMount, onDestroy } from 'svelte';
+	import { gsap } from 'gsap';
+	import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+	gsap.registerPlugin(ScrollTrigger);
+
+	let titles = [],
+		names = [],
+		triggers = [];
+
+	let container;
+
+	onMount(() => {
+		gsap.utils.toArray(titles).forEach((title, i) => {
+			let animation = gsap.from(title, {
+				duration: 0.5,
+				x: -100,
+				opacity: 0,
+				delay: i * 0.2,
+				scrollTrigger: {
+					trigger: container,
+					start: 'top 60%',
+					once: true
+				}
+			});
+
+			triggers.push(animation);
+		});
+
+		gsap.utils.toArray(names).forEach((name, i) => {
+			let animation = gsap.from(name, {
+				duration: 0.5,
+				x: 100,
+				opacity: 0,
+				delay: i * 0.2,
+				scrollTrigger: {
+					trigger: container,
+					start: 'top 60%',
+					once: true
+				}
+			});
+
+			triggers.push(animation);
+		});
+
+	});
+
+	onDestroy(() => {
+		triggers.forEach((trigger) => {
+			if (trigger.scrollTrigger) {
+				trigger.scrollTrigger.kill();
+			}
+			trigger.kill();
+		});
+	});
 </script>
 
-<div class="container">
-	{#each credits as credit}
-		<p><strong>{credit.title}</strong></p>
-		<p>{credit.names}</p>
+<div class="container" bind:this={container}>
+	{#each credits as credit, i}
+		<p bind:this={titles[i]}><strong>{credit.title}</strong></p>
+		<p bind:this={names[i]}>{credit.names}</p>
 	{/each}
 </div>
 

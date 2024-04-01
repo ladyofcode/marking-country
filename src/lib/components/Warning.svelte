69e633db09fdcf909df6dce34c4e38aee1c12ddb
ladyofcode <thelab@ladyofcode.com>
2024-04-01 16:59:32 +0100

    Update warning styles

--- src/lib/components/Warning.svelte
+++ src/lib/components/Warning.svelte
@@ -12,12 +12,23 @@
         align-items: center;
         justify-content: center;
         position: relative;
-        z-index: 50;
+        z-index: 20;
     }
 
     .warning-container div {
         width: 100%;
-        max-width: 800px;
+        max-width: var(--width-content);
         text-align: center;
+        padding: 0 var(--space-xl);
+    }
+
+    .warning-container div :global(p){
+        font-family: var(--font-serif);
+        /* font-weight: 700; */
+        font-size: var(--font-size-heading);
+    }
+
+    @media (min-width: 900px) {
+
     }
 </style>
